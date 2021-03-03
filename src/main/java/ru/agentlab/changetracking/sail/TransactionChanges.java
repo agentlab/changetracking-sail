677dd77077ee72b6dfe6945ae245f4e747bb5a9f
Dnnd <d.v.maslennikov@outlook.com>
2021-03-03 17:37:43 +0300

    implement hashCode & equals for TransactionChanges

--- src/main/java/ru/agentlab/changetracking/sail/TransactionChanges.java
+++ src/main/java/ru/agentlab/changetracking/sail/TransactionChanges.java
@@ -2,6 +2,7 @@ package ru.agentlab.changetracking.sail;
 
 import org.eclipse.rdf4j.model.Statement;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class TransactionChanges {
@@ -19,5 +20,20 @@ public class TransactionChanges {
 
     public Set<Statement> getRemovedStatements() {
         return removedStatements;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TransactionChanges that = (TransactionChanges) o;
+        return Objects.equals(addedStatements,
+                              that.addedStatements
+        ) && Objects.equals(removedStatements, that.removedStatements);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(addedStatements, removedStatements);
     }
 }
