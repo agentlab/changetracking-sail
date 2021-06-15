637cd6b8cd69b0fb385dcf2e8fc421129aa97d7c
Dnnd <d.v.maslennikov@outlook.com>
2021-06-15 19:42:54 +0300

    add stream<statements> overload to groupbysubject transformation

--- src/main/java/ru/agentlab/changetracking/filter/Transformations.java
+++ src/main/java/ru/agentlab/changetracking/filter/Transformations.java
@@ -1,18 +1,26 @@
 package ru.agentlab.changetracking.filter;
 
 
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.model.Resource;
-import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.*;
 import org.eclipse.rdf4j.model.impl.LinkedHashModel;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Transformations {
 
-    public static Map<IRI, Model> groupBySubject(Model statements) {
+    public static Map<IRI, Model> groupBySubject(Stream<Statement> statements) {
+        return statements.filter(statement -> statement.getSubject().isIRI())
+                         .collect(Collectors.groupingBy(
+                                 st -> (IRI) st.getSubject(),
+                                 HashMap::new,
+                                 Collectors.toCollection(LinkedHashModel::new)
+                         ));
+    }
+
+    public static Map<IRI, Model> groupBySubject(Iterable<Statement> statements) {
         Map<IRI, Model> groupedBySubject = new HashMap<>();
         for (Statement triple : statements) {
             Resource subject = triple.getSubject();
@@ -29,5 +37,4 @@ public class Transformations {
         }
         return groupedBySubject;
     }
-
 }
