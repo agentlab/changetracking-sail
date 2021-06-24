d390b056cbf2d016ac3de8b0830f181585712076
Dnnd <d.v.maslennikov@outlook.com>
2021-06-24 23:00:24 +0300

    migrate match to record

--- src/main/java/ru/agentlab/changetracking/filter/Match.java
+++ src/main/java/ru/agentlab/changetracking/filter/Match.java
@@ -1,19 +1,3 @@
 package ru.agentlab.changetracking.filter;
 
-public class Match<T> {
-    private final Filtering filteredFrom;
-    private final T data;
-
-    public Match(Filtering filteredFrom, T data) {
-        this.filteredFrom = filteredFrom;
-        this.data = data;
-    }
-
-    public Filtering getFilteredFrom() {
-        return filteredFrom;
-    }
-
-    public T getData() {
-        return data;
-    }
-}
+public record Match<T>(Filtering filteredFrom, T data) { }
