11d40822630b4332f3253778314d74b8bf0dd4e6
Dnnd <d.v.maslennikov@outlook.com>
2021-06-29 21:00:57 +0300

    Add utility grouping methods

--- src/main/java/ru/agentlab/changetracking/filter/Transformations.java
+++ src/main/java/ru/agentlab/changetracking/filter/Transformations.java
@@ -1,7 +1,10 @@
 package ru.agentlab.changetracking.filter;
 
 
-import org.eclipse.rdf4j.model.*;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.model.impl.LinkedHashModel;
 
 import java.util.HashMap;
@@ -18,6 +21,15 @@ public class Transformations {
                                  HashMap::new,
                                  Collectors.toCollection(LinkedHashModel::new)
                          ));
+    }
+
+
+    public static Iterable<Model> groupedBySubject(Stream<Statement> statements) {
+        return groupBySubject(statements).values();
+    }
+
+    public static Iterable<Model> groupedBySubject(Iterable<Statement> statements) {
+        return groupBySubject(statements).values();
     }
 
     public static Map<IRI, Model> groupBySubject(Iterable<Statement> statements) {
