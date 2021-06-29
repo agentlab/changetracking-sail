6b36984f251d4e72544dad665ae42136385c4d6e
Dnnd <d.v.maslennikov@outlook.com>
2021-06-29 21:00:41 +0300

    Add utility methods for match creation

--- src/main/java/ru/agentlab/changetracking/filter/Match.java
+++ src/main/java/ru/agentlab/changetracking/filter/Match.java
@@ -1,3 +1,11 @@
 package ru.agentlab.changetracking.filter;
 
-public record Match<T>(Filtering filteredFrom, T data) { }
+public record Match<T>(Filtering filteredFrom, T data) {
+    public static <T> Match<T> added(T data) {
+        return new Match<>(Filtering.ADDED, data);
+    }
+
+    public static <T> Match<T> removed(T data) {
+        return new Match<>(Filtering.REMOVED, data);
+    }
+}
